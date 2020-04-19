d0f11786ed1c53a67d8eb9c5aa8ed0eef5c0509b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-19 13:20:24 +0200

    remove project files

--- src/components/Tabs/index.js
+++
@@ -1,9 +0,0 @@
-// STEP 2: Create tabs
-// -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is resolved use console logs or breakpoints to review the structure.
-// Iterate over the topics creating a new tab for each topic, and appending it to the DOM
-// under the div.topics element.
-//
-//  Each tab should look like this:
-//    <div class="tab">topic here</div>
