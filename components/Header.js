cef82c74527ffc53e6cb53ec9b96b2c10ec59c31
David Veele <you@example.com>
2020-07-17 23:18:17 -0400

    header.js done

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // STEP 1: Create a Header component.
 // -----------------------
 // Write a function that returns the markup you see below:
@@ -11,4 +13,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  // I broke things up by element type because I got confused and kept missing parts. This method seems to work better for me.
+  const header = document.createElement("div");
+  header.classList.add("header");
+
+  const date = document.createElement("span");
+  date.classList.add("date");
+  date.textContent = "MARCH 28, 2020";
+
+  const title = document.createElement("h1");
+  title.textContent = "Lambda Times";
+
+  const temp = document.createElement("span");
+  temp.classList.add("temp");
+  temp.textContent = "98°";
+
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+
+  return header;
+}
+
+const headerCont = document.querySelector(".header-container");
+
+headerCont.appendChild(Header());
