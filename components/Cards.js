b1188e563e4d8f21ad54875d0b8452217b6a0759
Ryan Clark <rbclark244@gmail.com>
2020-07-20 14:29:56 -0400

    progressing through problem 3. changing objects to arrays repeatedly is key

--- components/Cards.js
+++ components/Cards.js
@@ -22,48 +22,262 @@
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 
 
-const articleMaker = (obj) => {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
+const articleMaker = (array, titles) => {
+    array.forEach(obj => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        
+        const headlineDiv = document.createElement('div');
+        headlineDiv.classList.add('headline');
+        headlineDiv.textContent = obj[titles] ['headline']
+        cardDiv.appendChild(headlineDiv)
     
-    const headlineDiv = document.createElement('div');
-    headlineDiv.classList.add('headline');
-    headlineDiv.textContent = obj['headline']
-    cardDiv.appendChild(headlineDiv)
-
-    const authorDiv = document.createElement('div');
-    authorDiv.classList.add('author');
-    cardDiv.appendChild(authorDiv);
-
-    const imgContainerDiv = document.createElement('div');
-    imgContainerDiv.classList.add('img-container');
-    authorDiv.appendChild(imgContainerDiv);
-
-    const authorImg = document.createElement('img');
-    authorImg.style.src = obj['authorPhoto']
-    imgContainerDiv.appendChild(authorImg);
-
-    const authorSpan = document.createElement('span');
-    authorSpan.textContent = `By ${obj['authorName']}`
+        const authorDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+        cardDiv.appendChild(authorDiv);
     
-
-
+        const imgContainerDiv = document.createElement('div');
+        imgContainerDiv.classList.add('img-container');
+        authorDiv.appendChild(imgContainerDiv);
+    
+        const authorImg = document.createElement('img');
+        authorImg.style.src = obj[titles]['authorPhoto']
+        imgContainerDiv.appendChild(authorImg);
+    
+        const authorSpan = document.createElement('span');
+        authorSpan.textContent = `By ${obj[titles]['authorName']}`
+        authorDiv.appendChild(authorImg);
+        
+        const articleAttach = document.querySelector('.cards-container')
+        articleAttach.appendChild(cardDiv)
+        console.log(articleAttach);
+        return articleAttach;
+    })
 }
+
 
 axios
     .get('https://lambda-times-backend.herokuapp.com/articles')
         .then( response => {
+            const articleTopic = response.data["articles"];
+            console.log(articleTopic);
+            const artValues = Object.values(articleTopic);
+            console.log(artValues[2]);
+            artValues.forEach( element => {
+                const artValuesHeadlineArray = Object.values(artValues);
+                console.log(artValues)
+                console.log(artValuesHeadlineArray);
+                console.log(artValuesHeadlineArray[0])
+
+                const headlinesArray = Object.values(artValuesHeadlineArray);
+                console.log(headlinesArray);
+
+                // indivArticles = artValuesHeadlineArray[element];
+                // indivArticles.forEach(headlines => {
+                //    const indivHeadlines = indivArticles[headlines];
+                //     return indivHeadlines;
+                // })
+            })
+            const artKeys = Object.keys(articleTopic);
+            console.log(artKeys);
+            console.log('success');
             console.log(response);
-
-         
-
-            return response;
         })
         .catch( error => {
-            console.log('error')
-            return error;
+            console.log('error');
         })
 
 
 
-        const articleInfo = response.data["articles"]["javascript"];
+
+
+
+
+
+
+
+
+
+
+
+
+// const articleMaker = (array) => {
+//     array.forEach(obj => {
+//         const cardDiv = document.createElement('div');
+//         cardDiv.classList.add('card');
+        
+//         const headlineDiv = document.createElement('div');
+//         headlineDiv.classList.add('headline');
+//         headlineDiv.textContent = obj['headline']
+//         cardDiv.appendChild(headlineDiv)
+    
+//         const authorDiv = document.createElement('div');
+//         authorDiv.classList.add('author');
+//         cardDiv.appendChild(authorDiv);
+    
+//         const imgContainerDiv = document.createElement('div');
+//         imgContainerDiv.classList.add('img-container');
+//         authorDiv.appendChild(imgContainerDiv);
+    
+//         const authorImg = document.createElement('img');
+//         authorImg.style.src = obj['authorPhoto']
+//         imgContainerDiv.appendChild(authorImg);
+    
+//         const authorSpan = document.createElement('span');
+//         authorSpan.textContent = `By ${obj['authorName']}`
+//         authorDiv.appendChild(authorImg);
+        
+//         const articleAttach = document.querySelector('.cards-container')
+//         articleAttach.appendChild(cardDiv)
+//         console.log(articleAttach);
+//         return articleAttach;
+//     })
+
+// }
+
+
+
+// const InfiniteArticles = () => {
+//     axios
+//         .get('https://lambda-times-backend.herokuapp.com/articles')
+//             .then( response => {
+//                 console.log(response);
+//                 const articleTopic = response.data["articles"];
+//                 console.log(articleTopic);
+//                 articleTopic.forEach(element => {
+//                     const indivTopics = response.data["articles"][element];
+//                     console.log(indivTopics);
+//                     indivTopics.forEach(thing => {
+//                         const topicSections = response.data["articles"][element][thing];
+//                         return topicSections;
+//                     })
+//                     return topicSections;
+//                 })
+//                 articleMaker(topicSections);
+//             })
+//             .catch( error => {
+//                 console.log('error')
+//                 return error;
+//             })
+// }
+
+// InfiniteArticles()
+
+
+
+
+// topicSections.forEach(obj => {
+//     const cardDiv = document.createElement('div');
+//     cardDiv.classList.add('card');
+//     console.log(cardDiv);
+    
+//     const headlineDiv = document.createElement('div');
+//     headlineDiv.classList.add('headline');
+//     headlineDiv.textContent = obj['headline']
+//     cardDiv.appendChild(headlineDiv)
+
+//     const authorDiv = document.createElement('div');
+//     authorDiv.classList.add('author');
+//     cardDiv.appendChild(authorDiv);
+
+//     const imgContainerDiv = document.createElement('div');
+//     imgContainerDiv.classList.add('img-container');
+//     authorDiv.appendChild(imgContainerDiv);
+
+//     const authorImg = document.createElement('img');
+//     authorImg.style.src = obj['authorPhoto']
+//     imgContainerDiv.appendChild(authorImg);
+
+//     const authorSpan = document.createElement('span');
+//     authorSpan.textContent = `By ${obj['authorName']}`
+//     authorDiv.appendChild(authorImg);
+
+//     console.log(cardDiv);
+// })
+
+// // const articleInfo = response.data["articles"]["javascript"];
+// // articleInfo.forEach(element => {
+    
+// // });
+
+
+
+
+// // const articleMaker = (obj) => {
+// //     const cardDiv = document.createElement('div');
+// //     cardDiv.classList.add('card');
+    
+//     const headlineDiv = document.createElement('div');
+//     headlineDiv.classList.add('headline');
+//     headlineDiv.textContent = obj['headline']
+//     cardDiv.appendChild(headlineDiv)
+
+//     const authorDiv = document.createElement('div');
+//     authorDiv.classList.add('author');
+//     cardDiv.appendChild(authorDiv);
+
+//     const imgContainerDiv = document.createElement('div');
+//     imgContainerDiv.classList.add('img-container');
+//     authorDiv.appendChild(imgContainerDiv);
+
+//     const authorImg = document.createElement('img');
+//     authorImg.style.src = obj['authorPhoto']
+//     imgContainerDiv.appendChild(authorImg);
+
+//     const authorSpan = document.createElement('span');
+//     authorSpan.textContent = `By ${obj['authorName']}`
+//     authorDiv.appendChild(authorImg);
+
+//     console.log(cardDiv);
+//     return cardDiv;
+// }
+
+
+// axios
+//     .get('https://lambda-times-backend.herokuapp.com/articles')
+//         .then( response => {
+//             console.log(response);
+//             return response;
+//         })
+//         .catch( error => {
+//             console.log('error')
+//             return error;
+//         })
+
+
+// const InfiniteArticles = () => {
+
+
+    
+//     const articleInfo = response.data["articles"]["javascript"];
+//     articleInfo.forEach(element => {
+        
+//     });
+// }
+
+
+// axios
+//     .get('https://lambda-times-backend.herokuapp.com/articles')
+//         .then( response => {
+//             const articleTopic = response.data["articles"];
+//             console.log(articleTopic);
+//             const artValues = Object.values(articleTopic);
+//             console.log(artValues);
+//             const artKeys = Object.keys(articleTopic);
+//             console.log(artKeys);
+//             console.log('success');
+//             console.log(response);
+//         })
+//         .catch( error => {
+//             console.log('error');
+//         })
+
+
+// axios
+//     .get('https://lambda-times-backend.herokuapp.com/articles')
+//         .then( response => {
+//             console.log('success');
+//         })
+//         .catch( error => {
+//             console.log('error');
+//         })
