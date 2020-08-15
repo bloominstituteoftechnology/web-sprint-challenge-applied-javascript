52f2aa2c8b965b8c7942c27f5cdd2c73aba76789
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-14 20:55:53 -0400

    Created Header function and appended to the DOM

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const head = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    head.appendChild(date);
+    head.appendChild(title);
+    head.appendChild(temp);
+
+    head.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = "MARCH 28, 2020";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    return head;
+};
+
+const entryHeader = document.querySelector('.header-container');
+
+entryHeader.appendChild(Header());
