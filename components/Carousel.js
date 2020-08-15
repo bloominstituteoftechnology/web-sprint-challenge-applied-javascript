06cf79190c8c5c00fef1c42f50332dc252730129
Michelle Rodriguez Harrison <dev@michellerh.com>
2020-08-15 15:00:38 -0400

    Working on Carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,87 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselImages = [
+  "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg",
+  "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg",
+  "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg",
+  "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg"
+]
+
+
+const carouselSection = document.querySelector('.carousel-container')
+
+const carouselComponent = (obj) => {
+  const carousel = document.createElement('div')
+  const leftButton = document.createElement('div')
+  const img1 = document.createElement('img')
+  const img2 = document.createElement('img')
+  const img3 = document.createElement('img')
+  const img4 = document.createElement('img')
+  const rightButton = document.createElement('div')
+
+
+  carousel.appendChild(leftButton)
+  carousel.appendChild(img1)
+  carousel.appendChild(img2)
+  carousel.appendChild(img3)
+  carousel.appendChild(img4)
+  carousel.appendChild(rightButton)
+
+  carousel.classList.add('carouselClass')
+  leftButton.classList.add('left-button')
+  rightButton.classList.add('right-button')
+
+  leftButton.textContent = "<"
+  rightButton.textContent = ">"
+
+  return carousel;
+}
+
+carouselSection.appendChild(carouselComponent());
+
+
+
+
+/**************** Carousel functionality **************/
+
+function reset(){
+  for(let i = 0; i < carouselImages.length; i++) {
+    sliderImages[i].style.display = 'none';
+  }
+}
+
+function startSlide(){
+  reset();
+  carouselImages[0].style.display = 'block';
+}
+
+function slideLeft(){
+  reset();
+  carouselImages[current - 1].style.display = 'block';
+  current--;
+}
+
+function slideRight(){
+  reset();
+  carouselImages[current + 1].style.display = 'block';
+  current++;
+}
+
+arrowRight.addEventListener('click', function(){
+  if(current === carouselImages.length - 1){
+    current = -1
+  }
+  slideRight();
+});
+
+
+arrowLeft.addEventListener('click', function() {
+  if(current === 0){
+      current = carouselImages.length;
+     }
+  slideLeft();
+});
+
+startSlide();
