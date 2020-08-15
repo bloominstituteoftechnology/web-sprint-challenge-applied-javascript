c1d304f01be152d34d402591e8055de7ec5cd1eb
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-14 21:21:42 -0400

    working on imgMaker, components/Cards.js

--- components/Cards.js
+++ components/Cards.js
@@ -21,6 +21,49 @@
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 
-// wow like the last assignment driving me nuts!
+// adding my get request and iterators for the Articles
 
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response=>{
+    Object.keys(response.data.articles).forEach(subject=>{
+        response.data.articles[subject].forEach(attribute=>{
+            document.querySelector('.cards-container').append(cardsMaker(attribute));
+        console.log(attribute);
+        });
+    });
+})
+.catch(error=>{console.log(error);
+});
 
+// creating elements and adding to the DOM
+
+function cardsMaker(data){
+    const divCardMaker=
+        document.createElement('div');
+    const divHeadlineMaker=
+        document.createElement('div');
+    const divAuthorMaker=
+        document.createElement('div');
+    const divImgMaker=
+        document.createElement('div');
+    const imgMaker=
+        document.createElement('img');
+    const spanMaker=
+        document.createElement('span');
+
+        divCardMaker.append(divHeadlineMaker);
+        divCardMaker.append(divAuthorMaker);
+        divAuthorMaker.append(divImgMaker);
+        divAuthorMaker.append(spanMaker);
+        divImgMaker.append(imgMaker);
+        divCardMaker.classList.add('card');
+        divHeadlineMaker.classList.add('headline');
+        divAuthorMaker.classList.add('author');
+        divImgMaker.classList.add('img-container');
+        divHeadlineMaker.textContent=data.headline;
+        imgMaker.src=data.authorPhoto;
+        spanMaker.textContent=data.authorName;
+        
+        console.log (data);
+        return divCardMaker;
+    };       
