23350a5ea870f14ad59b96bcc4a56ed8dabf4a6b
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-14 20:58:41 -0400

    created the cards function and appended all articles to the DOM

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,58 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const entryCards = document.querySelector('.cards-container');
+
+let info = [];
+
+axios.get(`https://lambda-times-api.herokuapp.com/articles`)
+.then((res) => {
+    console.log(res.data);
+    res.data.articles.bootstrap.forEach((art) => {
+        entryCards.append(cards(art));
+    });
+    res.data.articles.javascript.forEach((art) => {
+        entryCards.append(cards(art));
+    });
+    res.data.articles.jquery.forEach((art) => {
+        entryCards.append(cards(art));
+    });
+    res.data.articles.node.forEach((art) => {
+        entryCards.append(cards(art));
+    });
+    res.data.articles.technology.forEach((art) => {
+        entryCards.append(cards(art));
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+console.log(info);
+
+const cards = (art) => {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const authorImg = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(authorImg);
+    author.appendChild(authorName);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+
+    headline.textContent = art.headline;
+    authorImg.src = art.authorPhoto;
+    authorName.textContent = art.authorName;
+
+    card.addEventListener('click', () => {
+        console.log(headline.textContent);
+    })
+
+    return card;
+}
