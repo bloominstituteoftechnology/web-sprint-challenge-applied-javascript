5f94ecd525d4f2c530462a0fba7c304f669975ed
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-14 20:57:23 -0400

    Created the tabs function and added all the topics to the dom

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,26 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const topics = document.querySelector('.topics');
+
+axios.get(`https://lambda-times-api.herokuapp.com/topics`)
+.then((res) => {
+    console.log(res.data);
+    res.data.topics.forEach((top) => {
+        topics.append(tabs(top));
+    })
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const tabs = (topic) => {
+    const tab = document.createElement('div');
+
+    tab.classList.add('tab');
+
+    tab.textContent = topic;
+
+    return tab;
+};
