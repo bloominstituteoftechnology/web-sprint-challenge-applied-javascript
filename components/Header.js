a35004ff4d18b8a1cde5aaba60b5c795f64d38d9
Michelle Rodriguez Harrison <dev@michellerh.com>
2020-08-15 13:47:54 -0400

    Header complete

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container')
+
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    header.appendChild(date)
+    header.appendChild(h1)
+    header.appendChild(temp)
+
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    date.textContent = 'MARCH 28, 2019'
+    h1.textContent = 'Lambda Times'
+    temp.textContent = '98\xB0'
+
+    return header
+}
+
+headerContainer.appendChild(Header());
