be4bef07e7de38219b210d0558f3cb3139295330
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-04 10:05:30 -0600

    header code inside

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,33 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headercontainer = document.querySelector('.header-container')
+
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    header.classList.add('header')
+    date.classList.add('date')
+    title.classList.add('temp')
+
+    header.appendChild(header)
+    header.appendChild(date)
+    header.appendChild(title)
+    header.appendChild(temp)
+
+    return header
+}
+
+    headerData.forEach(headerObj => {
+        const headerElement = (headerObj)
+        headercontainer.appendChild(headerElement)
+    })
+    
+
+
+
+
+
