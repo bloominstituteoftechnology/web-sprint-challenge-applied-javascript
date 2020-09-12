79aedddde233835448d3f20e5db0a73e4d8af428
Aaron Flowers <flowersa29@gmail.com>
2020-09-12 11:33:42 -0500

    9/12

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //create element and class
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('header');
+
+
+    const headerSpan1 = document.createElement('span');
+    headerSpan1.classList.add('date');
+    headerSpan1.textContent= "March 28, 2020"
+    
+    const headerh1 = document.createElement('h1');
+    headerh1.textContent = "Lambda-times"
+
+    const headerspan2 = document.createElement('span')
+    headerspan2.classList.add('temp');
+    headerspan2.textContent = 98;
+
+    //appendChild 
+    headerDiv.appendChild(headerSpan1);
+    headerDiv.appendChild(headerh1);
+    headerDiv.appendChild(headerspan2);
+
+    return headerDiv;
+}
+
+        // add to page
+    document.querySelector('.header-container').appendChild(Header())
+    
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,58 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(Response => {
+        console.log(Response);
+        
+  
+    })
+     const NewTab = (tabs) => {
+     
+       const Javascript = document.createElement('div')
+    //    const bootstrapTab = document.createElement('div')
+      
+    //    const technologyTab = document.createElement('div')
+       
+    //    const jqueryTab = document.createElement('div')
+       
+    //    const nodejsTab = document.createElement('div')
+
+        Javascript.textContent = tabs
+        // bootstrapTab.textContent = "Bootstrap"
+        // technologyTab.textContent ="Technology"
+        // jqueryTab.textContent = "jquery"
+        // nodejsTab.textContent = "node.js"
+
+        Javascript.classList.add('tab')
+        // bootstrapTab.classList.add('tab')
+        // technologyTab.classList.add('tab')
+        // jqueryTab.classList.add('tab')
+        // nodejsTab.classList.add('tab')
+        
+        // Javascript.appendChild(bootstrapTab)
+        // Javascript.appendChild(technologyTab)
+        // Javascript.appendChild(jqueryTab)
+        // Javascript.appendChild(nodejsTab)
+
+
+
+       return Javascript;
+    }
+        document.querySelector('.topics').appendChild(NewTab());
+        
+    const entrypoint = document.querySelector('.topics')
+
+    axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(Response => {
+        console.log(Response);
+       
+        Response.data.topics.forEach(Element => {
+            const newtab1 = NewTab(Element);
+            entrypoint.appendChild(newtab1)
+            // console.log(newtab1)
+            console.log(` this is my element ${Element}`)
+        });
+        
+
+    })
