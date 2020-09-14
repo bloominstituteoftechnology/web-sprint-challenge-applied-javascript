ff69903554d941a0c7dedf9ebe520c596cc3746d
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-09-13 18:13:46 -0700

    created 1st div

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,9 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const divHeader = document.createElement("div")
+    divHeader.classList.add("header")
+
+    return divHeader
+}
