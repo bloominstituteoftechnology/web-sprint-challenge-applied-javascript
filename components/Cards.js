3df1f50ef40c1d643c23ccb407231f297c282635
Prince Iwejuo <princeiwejuo@gmail.com>
2020-10-02 13:54:05 -0400

    AppliedJavaSprint

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,43 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+const entryPoint3 = document.querySelector('.card')
+function cardMaker(){
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgCont = document.createElement('div')
+    const img = document.createElement('img src')
+    const name = document.createElement('span')
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgCont.classList.add('image-container')
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgCont)
+    imgCont.appendChild(img)
+    author.appendChild(name)
+
+    return card
+}
+
+ axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then( response => {
+    console.log(response.data)
+    console.log(response.data.message);
+    const info = Object.values(response.data.articles);
+    const cards = document.querySelector('.cards-container');
+    info.forEach( item => {
+        item.forEach(element => {
+            cards.appendChild(cardMaker(element))
+        })
+    })
+})
+.catch( error => {
+    console.log(error)
+})
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,34 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+const data = axios.get('https://lambda-times-api.herokuapp.com/topics')
+
+.then( response => {
+    console.log(response)
+})
+.catch( error => {
+    console.log(error)
+})
+
+const entryPoint2 = document.querySelector('.tabs')
+function Tabs(){
+    const js = document.createElement('javascript')
+    const bs = document.createElement('bootstrap')
+    const tech = document.createElement('technology')
+    const jquery = document.createElement('jquery')
+    const node = document.createElement('node.js')
+
+    js.classList.add('tabs')
+    bs.classList.add('tabs')
+    tech.classList.add('tabs')
+    jquery.classList.add('tabs')
+    node.classList.add('tabs')
+
+    return js
+
+    
+}
+const topics = document.querySelector('.topics')
+topics.appendChild(Tabs())
+
+console.log(Tabs())
