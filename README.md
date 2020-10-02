e43da9b9de717b14535517b95ed79530aed29218
chazkiker2 <chaz-kiker@lambdastudents.com>
2020-10-02 12:03:28 -0400

    Finish Header.js

--- README.md
+++ README.md
@@ -58,16 +58,16 @@ You are expected to be able to answer questions in these areas. Your responses c
 
 #### Launching the App
 
-- [ ] Navigate to the root of the project with your command line
-- [ ] Run `npm install` to download the dependencies listed in the `package.json` file
-- [ ] Run `npm start` to compile the project and serve it
-- [ ] Navigate Chrome to the URL indicated in the output of the `npm start` command
+- [x] Navigate to the root of the project with your command line
+- [x] Run `npm install` to download the dependencies listed in the `package.json` file
+- [x] Run `npm start` to compile the project and serve it
+- [x] Navigate Chrome to the URL indicated in the output of the `npm start` command
 
 ### Task 2: Project Requirements
 
 Your finished project must include all of the following requirements:
 
-- [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the `index.html` file itself.
+- [x] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the `index.html` file itself.
 
 - [ ] Following the instructions in the `components/Header.js` file, create the Header.
 
--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,86 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+
+function getDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  let month;
+  switch (mm) {
+    case "00": 
+      month = null;
+      break;
+    case "01": 
+      month = "January";
+      break;
+    case "02": 
+      month = "February";
+      break;
+    case "03": 
+      month = "March";
+      break;
+    case "04": 
+      month = "April";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "June";
+      break;
+    case "07":
+      month = "July";
+      break;
+    case "08":
+      month = "August";
+      break;
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
+    default: 
+      month = "Unknown";
+  }
+  const yyyy = String(today.getFullYear());
+  const datestring = `${month} ${dd}, ${yyyy}`;
+  return datestring;
+}
+
+
+function Header() {
+  // instantiate elements
+  const header = document.createElement("div");
+    const date = document.createElement("span");
+    const titleH1 = document.createElement("h1");
+    const temperature = document.createElement("span");
+  
+  // structure elements
+  header.append(date, titleH1, temperature);
+
+
+  // class names
+  header.classList.add("header");
+  date.classList.add("date");
+  date.textContent = getDate();
+  titleH1.textContent = "Lambda Times";
+  temperature.classList.add("temp");
+  temperature.textContent = "98°";
+
+  return header;
+
+}
+// Header();
+// console.log(12);
+// console.log(getDate());
+
+document.querySelector("div.header-container").appendChild(Header());
