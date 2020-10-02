5fce36c9fad494922814d7092761f1b52fe5a472
Jake Grella <jgrella13@gmail.com>
2020-10-02 10:20:42 -0500

    completed header construction

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+	//instantiate
+	const header = document.createElement('div');
+	const date = document.createElement('span');
+	const title = document.createElement('h1');
+	const temp = document.createElement('span');
+
+	//structure (append)
+	header.append(date);
+	header.append(title);
+	header.append(temp);
+
+	//class names
+	header.classList.add('header');
+	date.classList.add('date');
+	temp.classList.add('temp');
+
+	//text content
+	date.textContent = 'MARCH 28, 2020';
+	title.textContent = 'Lambda Times';
+	temp.textContent = '98°';
+
+	//return
+	return header;
+}
+
+console.log(Header());
