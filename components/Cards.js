7387aea1d38a55acf183f96b020e571c03d1db40
Sam Singer <sam.singet79@gmail.com>
2020-10-02 09:52:32 -0700

    MVP Complete

--- components/Cards.js
+++ components/Cards.js
@@ -19,4 +19,68 @@
 //
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
+
+const { default: Axios } = require("axios");
+
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+Axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(elementData => {
+let articleData = elementData.data.articles
+let jsData = articleData['javascript']
+let bootData = articleData['bootstrap']
+let techData = articleData['technology']
+let jqueryData = articleData['jquery']
+let nodeData = articleData['node']
+
+for(let i = 0, len=jsData.length; i<len; i++) {
+    articleMaker(jsData[i])
+}
+for(let i = 0, len=techData.length; i<len; i++) {
+    articleMaker(bootData[i])
+}
+for(let i = 0, len=techData.length; i<len; i++) {
+    articleMaker(techData[i])
+}
+for(let i = 0, len=jqueryData.length; i<len; i++) {
+    articleMaker(jqueryData[i])
+}
+for(let i = 0, len=nodeData.length; i<len; i++) {
+    articleMaker(nodeData[i])
+}
+console.log(articleData)
+console.log(jsData)
+ })
+.catch(err => {
+console.log('ERROR!')
+})
+
+
+
+function articleMaker(artObj) {
+    //HTMl INTERACTIOn
+    const cardContainer = document.querySelector('.cards-container')
+    //Instantiating
+    const cardDiv = document.createElement('div');
+    const headDiv = document.createElement('div');
+    const authorDiv = document.createElement('div')
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const spanName = document.createElement('span');
+    //Hierarchy
+    cardContainer.append(cardDiv)
+    cardDiv.append(headDiv);
+    cardDiv.append(authorDiv);
+    authorDiv.append(imgDiv);
+    cardDiv.append(spanName);
+    imgDiv.appendChild(img)
+    //ClassList
+    cardDiv.classList.add('card');
+    headDiv.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgDiv.classList.add('img-container');
+    //TextContnet
+    headDiv.textContent = artObj.headline
+    img.src = artObj.authorPhoto
+    spanName.textContent = artObj.authorName
+    return cardDiv
+}
