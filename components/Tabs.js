143201e0d024e631b80c0aee26f09f8fb8335137
chazkiker2 <chaz-kiker@lambdastudents.com>
2020-10-02 12:12:46 -0400

    finish Tabs.js

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +11,22 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// function createTabs() {
+  axios.get("https://lambda-times-api.herokuapp.com/topics")
+    .then(res => {
+      const divTopics = document.querySelector("div.topics");
+      const topicArr = res.data.topics;
+      // console.log(topicArr);
+      topicArr.forEach( x => {
+        const tab = document.createElement("div");
+        tab.classList.add("tab");
+        tab.textContent = x;
+        divTopics.appendChild(tab);
+      });
+    })
+    .catch(e => {
+      console.log(e);
+    });
+// }
+// createTabs();
