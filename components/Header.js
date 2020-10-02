e3ae858df86b53062079feb962f47ec974472307
Joe L <joseph.lasata@gmail.com>
2020-10-02 10:26:38 -0500

    Header Complete sans the degree symbol

--- components/Header.js
+++ components/Header.js
@@ -11,6 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //Instantiate Elements
+    const head = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+    //Add Classes 
+    head.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+    //Build our DOM Tree 
+    head.appendChild(date);
+    head.appendChild(title);
+    head.appendChild(temp);
+    //Populate Content
+    date.textContent = 'MARCH 28, 2020';
+    title.textContent = 'Lambda Times';
+    temp.textContent = `98 sym`;
 
-console.log(`I'm in the header`);
+    return head;
+}
+
+const headerClass = document.querySelector('.header-container');
+headerClass.append(Header());
+
