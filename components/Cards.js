4ac62058c15b2e3b2cc256248e6029390177c9b9
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-10-10 16:14:49 -0400

    tabs

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,11 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const articleMaker = (headline, authorPhoto, authorName) => {
+
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+
+}
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,37 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+const topics = (topic) => {
+
+    const topics = document.createElement('div');
+
+    topics.classList.add('tab');
+
+    topics.textContent = topic;
+
+    return topics;
+
+};
+
+const topicTab = document.querySelector('.topics');
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+
+    .then((successResponse) => {
+        console.log('tabs', successResponse);
+        successResponse.data.topics.forEach(url => {
+            const newTopic = topics(url)
+            topicTab.appendChild(newTopic);
+        })   
+    })
+
+    .catch((errorResponse) => {
+        console.log('error', errorResponse)
+    });
+
+    // const tabComponent = newTab();
+    // const tabContainer = document.querySelector('.topics');
+
+    // tabContainer.appendChild(tabComponent);
