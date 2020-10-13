16e7c6685822dd195a98142ecf59805c88efb75a
cvaldezo1 <cvaldez01@github.com>
2020-10-12 21:56:14 -0400

    cards almost done

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,58 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+        .then(res => {
+            console.log(res);
+            res.data.articles.bootstrap.forEach(item =>{
+                let ncard=cardCreator(item);
+                document.querySelector('.cards-container').appendChild(ncard);   
+            })
+            res.data.articles.javascript.forEach(item =>{
+                let ncard=cardCreator(item);
+                document.querySelector('.cards-container').appendChild(ncard);   
+            })
+            res.data.articles.jquery.forEach(item =>{
+                let ncard=cardCreator(item);
+                document.querySelector('.cards-container').appendChild(ncard);   
+            })
+            res.data.articles.node.forEach(item =>{
+                let ncard=cardCreator(item);
+                document.querySelector('.cards-container').appendChild(ncard);   
+            })
+            res.data.articles.technology.forEach(item =>{
+                let ncard=cardCreator(item);
+                document.querySelector('.cards-container').appendChild(ncard);   
+            })
+        
+           
+        })
+        .catch(err => {
+            console.log('axios is not working')
+        })
+        
+
+        function cardCreator(cardText){
+            let cardCreator = document.createElement('div');
+            cardCreator.textContent = cardText;
+            cardCreator.classList.add('card');
+
+            let headLine=document.createElement('div');
+            headLine.classList.add('headLine');
+            headLine.textContent=data['articles']['headLine'];
+
+
+            let author=document.createElement('div');
+            author.classList.add('author');
+            let image=document.createElement('img');
+            image.classList.add('img-container')
+            image.setAttribute('src',data['articles']['authorPhoto']);
+
+            let span =document.createElement('span');
+            span.textContent=data['authorName'];
+
+        
+            return cardCreator;
+        }
+
