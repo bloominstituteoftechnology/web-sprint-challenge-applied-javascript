f73f58df0f60fe71fc601fcc1ff9196353b5ab70
brennanlawrence <brennanlawrence2002@gmail.com>
2020-10-30 09:36:08 -0600

    Finished Tabs.js file

--- components/Tabs.js
+++ components/Tabs.js
@@ -8,4 +8,20 @@
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
 //
-// NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+// NOTE: you do _not_ need to install axios as it's included in the HTML via script element (Oops, just now seeing this)
+
+import axios from "axios";
+
+axios
+    .get("https://lambda-times-api.herokuapp.com/topics")
+    .then(function(response) {
+        response.data.topics.forEach(function(item) {
+            const tab = document.createElement("div");
+            tab.className = "tab";
+            tab.innerText = item;
+            document.querySelector(".topics").appendChild(tab);
+        })   
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
