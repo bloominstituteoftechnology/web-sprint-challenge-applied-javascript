8f16879de9e7530e75f0b2ef4cfe4fc449dc853b
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2020-10-30 13:53:10 -0400

    Finished

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const headDiv = document.createElement("div");
+    const date = document.createElement("span");
+    const title = document.createElement("h1");
+    const temp = document.createElement("span");
+
+    //add classes
+    headDiv.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+
+    //HTML structure
+    headDiv.appendChild(date);
+    headDiv.appendChild(title);
+    headDiv.appendChild(temp);
+
+    //add content
+    date.textContent = "March 28, 2020";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°"
+
+    return headDiv;
+
+}
+const container = document.querySelector(".header-container");
+
+container.appendChild(Header());
