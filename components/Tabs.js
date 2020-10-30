3c14f5b71e8ea8ea71ddb9b5bcf9da97b01f9804
Prince Iwejuo <princeiwejuo@gmail.com>
2020-10-30 13:50:21 -0400

    Unit2Sprint1

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,20 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+import axios from "axios"
+
+const entryPoint = document.querySelector('topics')
+    axios
+        .get('https://lambda-times-api.herokuapp.com/topics')
+        .then(res => {
+            console.log(res)
+        })
+        .catch( error => {
+            console.log(error)
+        })
+
+
+
+
