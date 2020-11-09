0a3e0ddbe5fbbefe66b460305b56842d3ea99c64
Andre Orellana <andre@Andres-MacBook-Pro.local>
2020-11-08 21:36:27 -0700

    working on header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(name, day, deg) {
+    const div = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+
+    title.textContent = name
+    date.textContent = day
+    temp.textContent = deg
+    div.style.width = '100%'
+    date.style.width = "100%"
+    temp.style.width = '100%'
+    div.style.display = 'flex'
+    div.style.justifyContent = "space-evenly"
+
+    div.appendChild(title)
+    div.appendChild(date)
+    div.appendChild(temp)
+
+
+    return div
+}
+
+document.querySelector('body').appendChild(Header('Lambda Times','MARCH 28, 2020','98°'))
