3643b06e7f1d458d1422cdb4860a319b18d8f2ed
xonxblue <aleshya.ab@gmail.com>
2020-11-08 22:42:51 -0500

    message1

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+
+
+ const thisIsHeader = (param) => {
+    // const header =document.createElement('div')
+    // const date = document.createElement('span')
+    // const title = document.createElement('h1')
+    // const degreeF= document.createElement('span')
+    
+    // header.classList.add('header')
+    // date.classList.add('date')
+    // degreeF.classList.add('temp')
+    
+    // header.textContent = 'Lambda Times'
+
+    // header.appendChild(date)
+    // date.appendChild(title)
+    // title.appendChild(degreeF)
+    
+ 
+   
+}
+
+const headerComponent = thisIsHeader()
+const headerContainer = componenet.querySelector('header-container')
+// Im looking for the index so i will get a return with my append method
+// 
+//headerContainer.append(headerComponent)
+console.log(headerContainer)
