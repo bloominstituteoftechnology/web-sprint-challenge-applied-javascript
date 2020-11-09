7b5ad44f0d24e555ab8a7c5a09b335dce66f9c5d
Loleatha <loleatha.joelle@gmail.com>
2020-11-08 22:46:27 -0500

    Finished header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function headerContainer() {
+    // Created header DOM components
+    const header = document.createElement(`div`);
+    const date = document.createElement(`span`);
+    const headerTitle = document.createElement(`h1`);
+    const temp = document.createElement(`span`);
+
+    // Classes
+    header.classList.add(`header`);
+    date.classList.add(`date`);
+    temp.classList.add(`temp`);
+
+    // Structure to look like HTML
+    header.appendChild(date);
+    header.appendChild(headerTitle);
+    header.appendChild(temp);
+
+    // textContent
+    date.textContent = "October 8, 2020";
+    headerTitle.textContent = "Lambda Times";
+    temp.textContent = `98°`;
+
+    // Return our function
+    return header;
+}
+
+const contain = document.querySelector(`div.header-container`);
+contain.append(headerContainer());
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,14 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+let topics = `tab`;
+
+axios
+.get(`https://lambda-times-api.herokuapp.com/topics`)
+.then(res => {
+    console.log(res);
+})
+.catch(err => {
+    console.log(`There is an error;`, err);
+});
