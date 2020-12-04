2988d0a0c1b5545ec0ef624a31184b2d5b4cfc41
Casey Rogers <casey-rogers@lamdastudents.com>
2020-12-04 10:53:47 -0800

    incomplete

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,50 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+
+function tabMaker(data){
+    
+    const div1 = document.createElement('div')
+    const div2 = document.createElement('div')
+    const div3 = document.createElement('div')
+    const div4 = document.createElement('div')
+    const div5 = document.createElement('div')
+
+    div1.append(div2)
+    div1.append(div3)
+    div1.append(div4)
+    div1.append(div5)
+
+    div1.classList.add(tab)
+    div2.classList.add(tab)
+    div3.classList.add(tab)
+    div4.classList.add(tab)
+    div5.classList.add(tab)
+
+    div1.textContent = data.topics[0];
+    div2.textContent = data.topics[1];
+    div3.textContent = data.topics[2];
+    div4.textContent = data.topics[3];
+    div5.textContent = data.topics[4];
+
+   return div1;
+
+
+}
+
+
+const tabs = document.querySelector('div.topics')
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+.then( response => {
+    
+    response.data.topics.forEach( item => {
+        let newTab = tabMaker(item);
+        tabs.appendChild(newTab);
+
+    })
+})
+.catch( error => {
+    console.log("Error: Topics.js");
+})
