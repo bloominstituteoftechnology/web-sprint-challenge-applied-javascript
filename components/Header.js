ca8b6d84f3abd7970c2f29cebe4b55f468fd5616
Lance Sanchez <lancesanchez@outlook.com>
2020-12-04 11:36:26 -0500

    Header completion

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,24 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // instantiating elements
+    const headerDiv = document.createElement("Div");
+    const dates = document.createElement("span");
+    const title = document.createElement("h1");
+    const temperature = document.createElement("span");
+    //setting class names
+    headerDiv.classList.add("header");
+    dates.classList.add("date");
+    title.textContent = "Lambda Times";
+    temperature.classList.add("temp");
+    temperature.textContent = "98";
+    //append elements
+    headerDiv.append(dates, title, temperature);
+    //return
+    return headerDiv;
+}
+
+document.querySelector("div.header-container").appendChild(Header());
+
+
