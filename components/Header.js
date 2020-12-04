cd3abc740b737864e7fd008311b917d0002e0db5
Lance Sanchez <lancesanchez@outlook.com>
2020-12-04 13:04:31 -0500

    completed

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,4 @@
-// STEP 1: Create a Header component.
-// -----------------------
-// Write a function that takes no arguments and returns the markup you see below:
-//
-//  <div class="header">
-//    <span class="date">MARCH 28, 2020</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div>
-//
-// Use your function to create a header
-// and append it to the DOM inside the div.header-container
+
 
 function Header() {
     // instantiating elements
