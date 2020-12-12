52ac6421c0704129c37ec925ca1da2f2f43603ac
Molly Brown <mollybrown0516@gmail.com>
2020-12-12 13:50:14 -0500

    completed header.js

--- README.md
+++ README.md
@@ -67,7 +67,7 @@ You are expected to be able to answer questions in these areas. Your responses c
 
 Your finished project must include all of the following requirements:
 
-- [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the `index.html` file itself.
+- [ x] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the `index.html` file itself.
 
 - [ ] Following the instructions in the `components/Header.js` file, create the Header.
 
@@ -89,6 +89,6 @@ After finishing your required elements, you can push your work further. These go
 
 Follow these steps for completing your project.
 
-- [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` branch (student's  Repo). **Please don't merge your own pull request**
-- [ ] Add your team lead as a reviewer on the pull-request
-- [ ] Your team lead will count the project as complete after receiving your pull-request
+- [x ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` branch (student's  Repo). **Please don't merge your own pull request**
+- [ x] Add your team lead as a reviewer on the pull-request
+- [x ] Your team lead will count the project as complete after receiving your pull-request
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 // STEP 1: Create a Header component.
 // -----------------------
-// Write a function that takes no arguments and returns the markup you see below:
+// [x]Write a function that takes no arguments and returns the markup you see below:
 //
 //  <div class="header">
 //    <span class="date">MARCH 28, 2020</span>
@@ -11,4 +11,37 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+
+
+function Header() {
+    const divHeader = document.createElement('div');
+    const spanElement = document.createElement('span');
+    const secondSpanElement = document.createElement('span')
+    const headingOneElement = document.createElement('h1');
+    
+    console.log(divHeader);
+    console.log(spanElement);
+    console.log(secondSpanElement);
+    console.log(headingOneElement);
+
+    divHeader.classList.add('header');
+    spanElement.classList.add('date');
+    secondSpanElement.classList.add('temp');
+    
+
+    divHeader.appendChild(spanElement);
+    divHeader.appendChild(headingOneElement);
+    divHeader.appendChild(secondSpanElement);
+
+    spanElement.textContent = 'MARCH 28, 2020';
+    headingOneElement.textContent = 'Lambda Times';
+    secondSpanElement.textContent = '98°';
+
+
+    return divHeader;
+}
+const createHeader = Header();
+console.log('it works', createHeader);
+
+const divHeaderContainer = document.querySelector('div.header-container');
+divHeaderContainer.appendChild(createHeader);
