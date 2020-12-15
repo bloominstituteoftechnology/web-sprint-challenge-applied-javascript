48c0aced6313c55ff97bafb9e937efc65657a6b7
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2020-12-15 16:18:59 -0700

    Project Complete

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,18 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+axios 
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then((success) => {
+        console.log(success);
+        success.data.topics.forEach((e) => {
+            const divTopic = document.createElement("div");
+            divTopic.classList.add("tab");
+            divTopic.textContent = e;
+            document.querySelector(".topics").appendChild(divTopic);
+        });
+    })
+    .catch((failure) => {
+        console.log(failure);
+    })
