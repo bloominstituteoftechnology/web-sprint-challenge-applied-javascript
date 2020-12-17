b44f60140c29ea009da9d630d2721f9ddbeff842
miketillman95 <matillman95@gmail.com>
2020-12-16 19:01:32 -0800

    update

--- components/Cards.js
+++ components/Cards.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
 
-function getArticles(articles){
+
+    console.log('started')
     axios.get(`https://lambda-times-api.herokuapp.com/articles`)
     .then( response => {
-        console.log(response)
-    }), 
-    (error) => {
+        console.log(response.data, "hello")
+    })
+   .catch (error => {
         console.log(error);
-    }
+    })
 
-}
 
-getArticles(articles)
 // STEP 3: Create article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-api.herokuapp.com/articles
@@ -35,3 +34,34 @@ getArticles(articles)
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+function Cards(object) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const headline = document.createElement('div')
+    headline.classList.add('headline')
+    headline.textContent = {}
+    card.appendChild(headline)
+
+    const author = document.createElement('div')
+    author.classList.add('author')
+    card.appendChild(author)
+
+    const imgContainer = document.createElement('div')
+    imgContainer.classList.add('img-container')
+    card.appendChild(imgContainer)
+
+    const img = document.createElement('img')
+    img.textContent = {}
+    imgContainer.appendChild(img)
+
+    const authorName = document.createElement('span')
+    authorName.textContent = `By `
+    card.appendChild(authorName)
+
+    headline.addEventListener('click', () => {
+        console.log()
+    })
+
+    return card
+}
