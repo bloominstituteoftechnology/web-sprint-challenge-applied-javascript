1e87575e1e36607f1d4dc4451c39413b49b08f24
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-01-15 12:58:24 -0500

    message

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,64 @@
+import axios from "axios";
+
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+	// TASK 5
+	// ---------------------
+	// Implement this function, which should return the markup you see below.
+	// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+	// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+	// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+	// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+	//
+	// <div class="card">
+	//   <div class="headline">{ headline }</div>
+	//   <div class="author">
+	//     <div class="img-container">
+	//       <img src={ authorPhoto }>
+	//     </div>
+	//     <span>By { authorName }</span>
+	//   </div>
+	// </div>
+	//
+	let cardUno = document.createElement("div");
+	let headlineDiv = document.createElement("div");
+	let authorDiv = document.createElement("div");
+	let imgContainer = document.createElement("div");
+	let imageElement = document.createElement("img");
+	let randSpan = document.createElement("span");
+
+	cardUno.appendChild(headlineDiv);
+	cardUno.appendChild(authorDiv);
+	authorDiv.appendChild(imgContainer);
+	imgContainer.appendChild(imageElement);
+	authorDiv.appendChild(randSpan);
+
+	cardUno.classList.add("card");
+	headlineDiv.classList.add("headline");
+	authorDiv.classList.add("author");
+	imgContainer.classList.add("img-container");
+
+	return cardUno;
+};
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+	axios
+		.get("https://lambda-times-api.herokuapp.com/topics")
+		.then((response) => {
+			// console.log(response.data.topics);
+			document.querySelector(selector).appendChild(Card(response.data.topics));
+		})
+		.catch((error) => {
+			console.log("something went wrong", error);
+		});
 
-export { Card, cardAppender }
+	// TASK 6
+	// ---------------------
+	// Implement this function that takes a css selector as its only argument.
+	// It should obtain articles from this endpoint: `c`
+	// However, the articles do not come organized in a single, neat array. Inspect the response closely!
+	// Create a card from each and every article object in the response, using the Card component.
+	// Append each card to the element in the DOM that matches the selector passed to the function.
+	//
+};
+
+export { Card, cardAppender };
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,64 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
-}
+	// TASK 3
+	// ---------------------
+	// Implement this function which takes an array of strings ("topics") as its only argument.
+	// As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+	// then the function returns the markup below.
+	// The tags used, the hierarchy of elements and their attributes must match the provided markup!
+	// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+	//
+	// <div class="topics">
+	//   <div class="tab">javascript</div>
+	//   <div class="tab">bootstrap</div>
+	//   <div class="tab">technology</div>
+	// </div>
+	let mainDiv = document.createElement("div");
+	mainDiv.classList.add("topics");
+	topics.forEach((topic) => {
+		const topicdiv = document.createElement("div");
+		mainDiv.append(topicdiv);
+		topicdiv.classList.add("tab");
+		topicdiv.textContent = topic;
+	});
+
+	return mainDiv;
+};
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
+	axios
+		.get("https://lambda-times-api.herokuapp.com/topics")
+		.then((response) => {
+			// console.log(response.data.topics);
+			document.querySelector(selector).appendChild(Tabs(response.data.topics));
+		})
+		.catch((error) => {
+			console.log("something went wrong", error);
+		});
+};
 
-export { Tabs, tabsAppender }
+// TASK 4
+// ---------------------
+// Implement this function which takes a css selector as its only argument.
+// It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
+// Find the array of topics inside the response, and create the tabs using the Tabs component.
+// Append the tabs to the element in the DOM that matches the selector passed to the function.
+//
+
+// .get("https://lambda-times-api.herokuapp.com/topics")
+// 	.then((response) => {
+// 		return response.data;
+// 		console.log(response.data);
+// 	})
+// 	.then((data) => {
+// 		const tabData = document.querySelector(selector);
+
+// 		tabData.appendChild(Tabs(data));
+// 	})
+// 	.catch((error) => {
+// 		console.log("something went wrong", error);
+// 	});
+
+// console.log(selector);
+export { Tabs, tabsAppender };
