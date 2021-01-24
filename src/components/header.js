6c59b7c7ccf6c547a0d1942c5d5a523e88863bd4
andrewroti <andrewroti@gmail.com>
2021-01-24 02:51:20 -0500

    almost done with header

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,23 @@ const Header = (title, date, temp) => {
   //  </div>
   //
   const header = document.createElement('div');
-  header.textContent = "hi";
+  header.classList.add = ('header');
+  
+  const headerDate = document.createElement('span');
+  headerDate.classList.add('date');
+  headerDate.textContent = date;
+  header.appendChild(headerDate);
+  
+  const headerTitle = document.createElement('h1');
+  headerTitle.textContent = title;
+  header.appendChild(headerTitle);
+  
+  const headerTemp = document.createElement('span');
+  headerTemp.classList.add('temp');
+  headerTemp.textContent = temp;
+  header.appendChild(headerTemp);
+
+  return header;
 }
 
 const headerAppender = (selector) => {
@@ -22,6 +38,10 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const selectorParent = document.querySelector(selector);
+  return selectorParent.appendChild(Header("Lambda Times", "Jan 24, 2021", "39*F"));
+
 }
+
 
 export { Header, headerAppender }
