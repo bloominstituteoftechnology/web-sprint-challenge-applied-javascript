0679147392ed5d8be4f1be3d85fbf48757e1e702
andrewroti <andrewroti@gmail.com>
2021-01-24 02:12:23 -0500

    testing codegrade

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const header = document.createElement('div');
 }
 
 const headerAppender = (selector) => {
