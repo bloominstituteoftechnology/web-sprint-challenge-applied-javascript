a5d6e8ee4b851ca7a322d8145c9b4994f3500832
Aleksandr Ardanov <alex@air-aleksandr.lan>
2021-02-12 11:13:21 -0600

    mvp done

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,7 +19,37 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+//! create elements  
+const card = document.createElement('card');
+const headline = document.createElement('div');
+const author = document.createElement('div');
+const imgContainer = document.createElement('div');
+const img = document.createElement('img');
+const span = document.createElement('span');
+//! add classes to elements
+card.classList.add('card');
+headline.classList.add('headline');
+author.classList.add('author');
+imgContainer.classList.add('img-container');
+//! add link to image
+img.src = article.authorPhoto;
+//! add text content
+headline.textContent = article.headline;
+span.textContent = `By ${article.authorName}`;
+//! create hierarchy
+card.appendChild(headline);
+card.appendChild(author);
+author.appendChild(imgContainer);
+imgContainer.appendChild(img);
+author.appendChild(span);
+//! create eventListener 
+card.addEventListener('click', () => {
+ console.log(headline);
+})
+//! return 
+return card;
 }
+
 
 const cardAppender = (selector) => {
   // TASK 6
@@ -27,7 +59,41 @@ const cardAppender = (selector) => {
   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
+  
+  axios
+  .get('https://lambda-times-api.herokuapp.com/articles')
+  .then(res => {
+    const select = document.querySelector(selector);
+    //! receive an object with 5 arrays inside
+    const arrayArticles = res.data.articles;
+    // console.log(arrayArticles); //!check array
+    //! create small arrays from the object
+    const javascript = arrayArticles.javascript;
+    // console.log(javascript) //! check small array
+    const bootstrap = arrayArticles.bootstrap;
+    const technology = arrayArticles.technology;
+    const jquery = arrayArticles.jquery;
+    const node = arrayArticles.node;
+    //! add cards to the page
+    javascript.forEach(item =>{
+      select.appendChild(Card(item))
+    })
+    bootstrap.forEach(item =>{
+      select.appendChild(Card(item))
+    })
+    technology.forEach(item =>{
+      select.appendChild(Card(item))
+    })
+    jquery.forEach(item =>{
+      select.appendChild(Card(item))
+    })
+    node.forEach(item =>{
+      select.appendChild(Card(item))
+    })
+  })
+  .catch(err =>{
+    console.log(err);
+  })
 }
 
 export { Card, cardAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,7 +15,28 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topicsMain = document.createElement('div');
+  topicsMain.classList.add('topics');
+  const array = Array.from(topics.topics);
+  // console.log(array) //! i checked array here
+
+  //! create div for each string from 'topics'
+   const div = array.map((topic) => {
+   const topicCreate = document.createElement('div');
+   topicCreate.classList.add('tab');
+   topicCreate.textContent = topic;
+   return topicCreate;
+  })
+  //! create hierarchy for created divs and main div
+   div.forEach(div => {
+     topicsMain.appendChild(div);
+   })
+
+   return topicsMain;
 }
+//!check function
+// const a = ['it','should','work'];
+// console.log(Tabs(a))
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -23,6 +46,17 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  axios
+  .get('https://lambda-times-api.herokuapp.com/topics')
+  .then(res => {
+    const final = Tabs(res.data);
+    const select = document.querySelector(selector);
+    select.appendChild(final);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 }
 
 export { Tabs, tabsAppender }
