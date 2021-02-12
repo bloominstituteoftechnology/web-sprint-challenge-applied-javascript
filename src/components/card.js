64a5092734029b40f31266b29f3bf375479550fd
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-02-12 11:02:52 -0500

    finished sprint project

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,3 @@
-const Card = (article) => {
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,17 +16,87 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
 
-const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+  import axios from "axios";
 
-export { Card, cardAppender }
+  const Card = (article) => {
+    const cardHolder = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgHolder = document.createElement('div');
+    const img = document.createElement('img');
+    const closure = document.createElement('span');
+  
+    cardHolder.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgHolder.classList.add('img-container');
+    img.src = article.authorPhoto;
+  
+    cardHolder.appendChild(headLine);
+    cardHolder.appendChild(author);
+    author.appendChild(imgHolder);
+    imgHolder.appendChild(img);
+    author.appendChild(closure);
+    
+    headLine.textContent = article.headline;
+    closure.textContent = `By ${article.authorName}`
+  
+    cardHolder.addEventListener('click', (event) => {
+      console.log(article.headline)
+    })
+    return cardHolder;
+  
+  }
+  
+    // TASK 6
+    // ---------------------
+    // Implement this function that takes a css selector as its only argument.
+    // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
+    // However, the articles do not come organized in a single, neat array. Inspect the response closely!
+    // Create a card from each and every article object in the response, using the Card component.
+    // Append each card to the element in the DOM that matches the selector passed to the function.
+    //
+  
+  const cardAppender = (selector) => {
+    axios
+    .get("https://lambda-times-api.herokuapp.com/articles")
+    .then((response) => {
+      const javascript = response.data.articles.javascript;
+      const bootstrap = response.data.articles.bootstrap;
+      const technology = response.data.articles.technology;
+      const node = response.data.articles.node;
+      const jquery = response.data.articles.jquery;
+      
+      const newSelector = document.querySelector(selector);
+      
+        javascript.forEach((item) => {
+          const newCard = Card(item);
+          newSelector.append(newCard);
+        })
+  
+        bootstrap.forEach((item) => {
+          const newCard = Card(item);
+          newSelector.append(newCard);
+        })
+  
+        technology.forEach((item) => {
+        const newCard = Card(item);
+        newSelector.append(newCard);
+  })
+  
+        jquery.forEach((item) => {
+          const newCard = Card(item);
+          newSelector.append(newCard);
+        })
+  
+      node.forEach((item) => {
+        const newCard = Card(item);
+        newSelector.append(newCard);
+      })
+  
+      
+    })
+  }
+  
+  export { Card, cardAppender }
