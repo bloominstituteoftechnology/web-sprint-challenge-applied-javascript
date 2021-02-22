b135a30de75bf8e00f8898c25e0237a136aac1c0
unknown <victoria90dupont@gmail.com>
2021-02-21 19:18:00 -0800

    tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -13,6 +13,34 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topic = document.createElement('div')
+  const tab1 = document.createElement('div')
+  const tab2 = document.createElement('div')
+  const tab3 = document.createElement('div')
+
+  topic.appendChild(tab1)
+  topic.appendChild(tab2)
+  topic.appendChild(tab3)
+  
+  topic.classList.add('topics')
+  tab1.classList.add('tab1')
+  tab2.classList.add('tab2')
+  tab3.classList.add('tab3')
+
+  topic.textContent = topics
+  tab1.textContent = tab1
+  tab2.textContent = tab2
+  tab3.textContent = tab3
+
+  return topic
+
+
+
+
+
+
+
+
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +51,9 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const elDOM = document.createElement('div')
+  elDOM.classList.add(selector)
+  return elDOM.appendChild(Tabs(src = `https://lambda-times-api.herokuapp.com/topics`))
 }
 
 export { Tabs, tabsAppender }
