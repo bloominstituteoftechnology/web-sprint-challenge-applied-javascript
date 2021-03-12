14a2f7ebd6811cc38e3087838c3bca5f7573a50a
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-12 09:55:39 -0500

    adds tabs successfully to page

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -23,6 +25,8 @@ const Tabs = (topics) => {
     singleTopicDiv.textContent = topic;
     topicDiv.appendChild(singleTopicDiv);
   });
+
+  return topicDiv;
 }
 
 const tabsAppender = (selector) => {
@@ -33,6 +37,23 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const tabs = document.querySelector(selector);
+
+
+  axios
+    .get(`https://lambda-times-api.herokuapp.com/topics`)
+    .then((res) => {
+      // console.log(res.data)
+      const topicsArray = res.data.topics;
+      console.log(topicsArray);
+      const topicDiv = Tabs(topicsArray);
+      console.log(topicDiv);
+      tabs.appendChild(topicDiv);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
 }
 
 export { Tabs, tabsAppender }
