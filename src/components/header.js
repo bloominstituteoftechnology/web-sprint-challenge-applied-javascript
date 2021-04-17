87ceade2a0e6ae1c840a6e1312a3af410a451d52
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2021-04-16 20:37:54 -0400

    header complete

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,14 @@ const Header = (title, date, temp) => {
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
   //
+const header = document.createElement('div')
 const headTitle = document.createElement('h1');
 const headDate = document.createElement('span');
 const headTemp = document.createElement('span');
+
+header.appendChild(headTitle)
+header.appendChild(headDate)
+header.appendChild(headTemp)
 
 headTitle.textContent = title
 headDate.textContent = date
@@ -20,6 +25,9 @@ headTemp.textContent = temp
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  return header
+  
 }
 
 const headerAppender = (selector) => {
@@ -29,6 +37,11 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const mainHeader = document.querySelector('.header-container')
+
+  Header().classList.add(selector)
+ 
+  mainHeader.appendChild(headerAppender())
 }
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -7,6 +7,15 @@ const Tabs = (topics) => {
   // The tags used, the hierarchy of elements and their attributes must match the provided markup!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
   //
+  const mainTopics = document.createElement('div')
+  const tab1 = document.createElement('div')
+  const tab2 = document.createElement('div')
+  const tab3 = document.createElement('div')
+
+  tab1.textContent = topics
+  tab2.textContent = topics
+  tab3.textContent = topics
+
   // <div class="topics">
   //   <div class="tab">javascript</div>
   //   <div class="tab">bootstrap</div>
@@ -23,6 +32,8 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  Tabs().classLiss.add(selector)
 }
 
 export { Tabs, tabsAppender }
