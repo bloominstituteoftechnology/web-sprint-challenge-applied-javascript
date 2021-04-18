fe3e44c5fc5b18a3bd14b8fcc554b486847a77c6
Dan Shaw <Dan@Dans-MacBook-Pro.local>
2021-04-18 09:07:25 -0400

    completed header, card, and tabs files

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,32 @@
+
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+
+  const mainHeader = document.createElement('div')
+  const dateSpan = document.createElement('span')
+  const h1Title = document.createElement('h1')
+  const tempSpan = document.createElement('span')
+
+  mainHeader.classList.add('header')
+  dateSpan.classList.add('date')
+  tempSpan.classList.add('temp')
+
+  dateSpan.textContent = `Published: ${date}`
+  h1Title.textContent = title
+  tempSpan.textContent = `Temperature: ${temp}`
+
+  mainHeader.appendChild(dateSpan)
+  mainHeader.appendChild(h1Title)
+  mainHeader.appendChild(tempSpan)
+
+  return mainHeader
+
 }
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+
+  let entry = document.querySelector(selector)
+  entry.appendChild(Header('The NY Times', '3/17/21', '52 Degrees'))
+  
 }
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,33 @@
+import axios from 'axios'
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+
+  const mainTab = document.createElement('div')
+
+  mainTab.classList.add('topics')
+
+  topics.forEach(topic => {
+    let tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent = topic
+    mainTab.appendChild(tab)
+  })
+
+  return mainTab
+  
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+
+  let entry = document.querySelector(selector)
+
+  axios.get('https://lambda-times-api.herokuapp.com/topics')
+  .then(res => {
+    let newTab = Tabs(res.data.topics)
+    entry.appendChild(newTab)
+  })
+  .catch(err => console.log(err))
+  
 }
 
 export { Tabs, tabsAppender }
