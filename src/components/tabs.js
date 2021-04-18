2950fa6c70f3e3b6e24c4d5d9a1e57339ebfee05
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-18 14:43:27 -0500

    decided to add an if statement to task 2 to prevent errors

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -13,7 +13,7 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-  
+
 }
 
 const tabsAppender = (selector) => {
@@ -24,6 +24,7 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  
 }
 
 export { Tabs, tabsAppender }
