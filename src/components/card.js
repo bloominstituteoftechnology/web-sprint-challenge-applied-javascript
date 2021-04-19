b0eefdac8bf575ab624a373cc934779708a71ec2
Owen  Damron <damronowen@yahoo.com>
2021-04-18 22:05:20 -0500

    finished card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,4 @@
-const Card = (article) => {
-  // TASK 5
+// TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
   // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
@@ -17,10 +16,41 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+
+import axios from "axios"
+
+const Card = (article) => {
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imgContainer = document.createElement('div')
+  const img = document.createElement('img')
+  const authorName = document.createElement('span')
+
+  card.classList.add('card')
+  headline.classList.add('headline')
+  author.classList.add('author')
+  imgContainer.classList.add('img-container')
+
+  card.appendChild(headline)
+  card.appendChild(author)
+  author.appendChild(imgContainer)
+  author.appendChild(authorName)
+  imgContainer.appendChild(img)
+
+  headline.textContent = article.headline
+  authorName.textContent = `By: ${article.name}`
+  img.setAttribute('src', `${article.authorPhoto}`)
+  
+  card.addEventListener('click', (event) => {
+    console.log(headline)
+  })
+
+  return card
+  
 }
 
-const cardAppender = (selector) => {
-  // TASK 6
+// TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
   // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
@@ -28,6 +58,22 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+
+const cardAppender = (selector) => {
+  const array = ['javascript', 'bootstrap', 'technology', 'jquery', 'node']
+  axios.get(`https://lambda-times-api.herokuapp.com/articles`)
+  .then((res) => {
+    array.forEach(topic => {
+      res.data.articles[topic].forEach(el => {
+        const newDiv = Card(el)
+        document.querySelector(selector).appendChild(newDiv)
+      })
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  
 }
 
 export { Card, cardAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,5 +1,4 @@
-const Tabs = (topics) => {
-  // TASK 3
+ // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
   // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
@@ -13,16 +12,44 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+
+import axios from "axios"
+
+
+const Tabs = (topics) => {
+  const topicsContainer = document.createElement('div')
+  topicsContainer.classList.add('topics')
+
+  topics.forEach((topic) => {
+    const tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent = topic
+    topicsContainer.appendChild(tab)
+  })
+  
+  return topicsContainer
+ 
 }
 
-const tabsAppender = (selector) => {
-  // TASK 4
+// TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
   // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+const tabsAppender = (selector) => {
+  const a = document.querySelector(selector)
+
+  axios.get(`https://lambda-times-api.herokuapp.com/topics`)
+  .then(response => {
+    console.log(response)
+    const tabs = Tabs(response.data.topics)
+    a.appendChild(tabs)
+  })
+  
+  
 }
 
 export { Tabs, tabsAppender }
