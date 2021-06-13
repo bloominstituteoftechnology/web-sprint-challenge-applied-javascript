f6f5ccb4df01fde7b49218231c696cd0aa0e66a7
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-06-13 18:29:02 -0400

    task 3 and 4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,26 @@
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
+  const topicBody = document.createElement('div')
+  topicBody.classList.add('topics')
+  
+  topics.forEach((topics) => {
+    const thing = document.createElement('div')
+    thing.classList.add('tab')
+    thing.textContent = topics
+    
+    topicBody.appendChild(thing)
+    
+  })
+  console.log(topicBody)
+  return topicBody
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+  axios.get('https://lambda-times-api.herokuapp.com/topics')
+   .then(response => {
+  const mountingpoint = document.querySelector(selector)
+  const data = Tabs(response.data.topics)
+  mountingpoint.appendChild(data)
+})
 }
 
 export { Tabs, tabsAppender }
