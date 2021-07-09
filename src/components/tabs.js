bd406615f08a9c8c2981a57915a86d9a38721fe6
marciemaclean <macleanphoenix@gmail.com>
2021-07-09 16:55:42 -0400

    task 3 completed

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,14 @@
 const Tabs = (topics) => {
+  const topicsDiv = document.createElement('div')
+  topicsDiv.classList.add('topics')
+  topics.forEach(item =>{
+    const tabDiv = document.createElement('div')
+    tabDiv.textContent = item
+    tabDiv.classList.add('tab')
+    topicsDiv.appendChild(tabDiv)
+  })
+  return topicsDiv
+}
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -13,9 +23,11 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
 
 const tabsAppender = (selector) => {
+
+}
+
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -23,6 +35,4 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
-
 export { Tabs, tabsAppender }
