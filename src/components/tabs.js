88015a57bc63b82128380c2bd53c3abacb07a29e
Lambda Account <lambdaschool@ZachsMacBook.local>
2021-07-09 16:22:46 -0600

    first push

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -7,12 +7,22 @@ const Tabs = (topics) => {
   // The tags used, the hierarchy of elements and their attributes must match the provided markup!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
   //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
+   <div class="topics">
+     <div class="tab">javascript</div>
+     <div class="tab">bootstrap</div>
+     <div class="tab">technology</div>
+   </div>
   //
+  
+  const topicsContainer = document.createElement('div');
+  topicsContainer.classList.add('topics');
+  topics.forEach((topic) =>{
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    topicsContainer.appendChild(tab)
+  });
+  return topicsContainer
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +33,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  const el = document.querySelector(selector)
+  axios.get(`http://localhost:5000/api/topics`);
+    then(response =>{
+    console.log(response);
+    const tabs = Tabs(response.data.topics);
+    el.appendChild(tabs);
+  });
 }
 
 export { Tabs, tabsAppender }
