b5da875c3cd46138cc01a8a4ec5c281d829588ac
kennymiesner <kennymiesner@gmail.com>
2021-07-09 11:18:36 -0500

    Built header task 1

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,30 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  const header = document.createElement('div')
+  const headerDate = document.createElement('span')
+  const headerTitle = document.createElement('h1')
+  const headerTemp = document.createElement('span')
+
+  header.appendChild(headerDate)
+  header.appendChild(headerTitle)
+  header.appendChild(headerTemp)
+
+  header.classList.add('header')
+  headerDate.classList.add('date')
+  headerTemp.classList.add('temp')
+
+  headerDate.textContent = `${date}`
+  headerTitle.textContent = `${title}`
+  headerTemp.textContent = `${temp}`
+
+  return header
+
 }
+
+console.log(Header)
+
 
 const headerAppender = (selector) => {
   // TASK 2
