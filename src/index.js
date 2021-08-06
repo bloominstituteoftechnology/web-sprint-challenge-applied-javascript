03301f06e7bb4d0877b75a3fe9efa53431226623
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-08-06 09:23:10 -0700

    completed header tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // DO NOT MAKE CHANGES TO THIS FILE
 // DO NOT MAKE CHANGES TO THIS FILE
 // DO NOT MAKE CHANGES TO THIS FILE
-import { cardAppender } from './components/card'
-import { tabsAppender } from './components/tabs'
-import { headerAppender } from './components/header'
+import { cardAppender } from './components/card';
+import { tabsAppender } from './components/tabs';
+import { headerAppender } from './components/header';
 
-headerAppender('.header-container')
-tabsAppender('.tabs-container')
-cardAppender('.cards-container')
+headerAppender('.header-container');
+tabsAppender('.tabs-container');
+cardAppender('.cards-container');
 
-console.log('Lambda Times loaded!')
+console.log('Lambda Times loaded!');
