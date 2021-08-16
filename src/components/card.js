1dbae2ace0259e74d41a2820c14b950e13dbba59
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-08-15 20:54:22 -0400

    code grade setup

--- src/components/card.js
+++ src/components/card.js
@@ -20,7 +20,7 @@ const Card = (article) => {
 }
 
 const cardAppender = (selector) => {
-  // TASK 6
+  // TASK 6  Setting up codegrade
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
   // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
