14861b8c596e163f0a0ee67143f6febdf3645fcf
Cori Wooley <wooleycori@gmail.com>
2022-02-11 04:18:49 -0800

    debugged classlist. completed task 5

--- src/components/card.js
+++ src/components/card.js
@@ -8,32 +8,26 @@ const Card = (article) => {
 
   cardDiv.classList.add('card')
   cardHeadline.classList.add('headline')
-  authorInfo.classList('author')
-  photoCont.classList('img-container')
+  authorInfo.classList.add('author')
+  photoCont.classList.add('img-container')
 
   cardHeadline.textContent = article.headline
   name.textContent = `By ${article.authorName}`
 
   authorPhoto.src = article.authorPhoto
 
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
+  cardDiv.appendChild(cardHeadline)
+  cardDiv.appendChild(authorInfo)
+  authorInfo.appendChild(photoCont)
+  photoCont.appendChild(authorPhoto)
+  authorInfo.appendChild(name)
+
+  cardDiv.addEventListener('click', () => {
+    console.log(cardHeadline)
+  })
+
+  return cardDiv
+
 }
 
 const cardAppender = (selector) => {
