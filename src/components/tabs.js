7cf9554898b44cc493271246b454a650fc316319
Cori Wooley <wooleycori@gmail.com>
2022-02-11 03:53:55 -0800

    created functioning tabs appender

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Tabs = (topics) => {
   const topicDiv = document.createElement('div')
 
@@ -18,6 +20,16 @@ const Tabs = (topics) => {
 }
 
 const tabsAppender = (selector) => {
+  axios.get("http://localhost:5000/api/topics")
+  .then(resp => {
+    const topicArr = resp.data.topics
+    document.querySelector(selector).appendChild(Tabs(topicArr))
+
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -25,6 +37,17 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+
+//   const topicArr = Object.keys(resp.data.message)
+//   for (let i = 0; i < topicArr.length; i++){
+//     const topicTab = document.querySelector(selector).appendChild(Tabs(topicArr.topics[i]))
+//     return topicTab
+//   }
+//   })
+//  .catch(err => {
+//    console.log(err)
+//  })
+
+
 
 export { Tabs, tabsAppender }
