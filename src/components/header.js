64a1e47bd1ecf19e17955d68eb544b739a299021
Cori Wooley <wooleycori@gmail.com>
2022-02-11 01:37:06 -0800

    header textcontent and append child. task 1 complete

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,20 @@ const header = document.createElement('div')
 const headerDate = document.createElement('span')
 const headerTitle = document.createElement('h1')
 const headerTemp = document.createElement('span')
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+
+header.classList.add('header')
+headerDate.classList.add('date')
+headerTemp.classList.add('temp')
+
+header.appendChild(headerDate)
+header.appendChild(headerTitle)
+header.appendChild(headerTemp)
+
+headerDate.textContent = date
+headerTitle.textContent = title
+headerTemp.textContent = temp
+
+return header
 }
 
 const headerAppender = (selector) => {
