7546ab2db5c0f5fd6cd2ec78dba7ae30a010465e
Casey Harding <casey.r.harding@gmail.com>
2022-03-03 12:10:36 -0600

    Hotfix to account for new jest requirements with the updated version.

--- jest.config.js
+++ jest.config.js
@@ -128,6 +128,8 @@ module.exports = {
   "setupFiles": [
     "./jest.setup.js"
   ],
+  
+  "testEnvironment": "jsdom"
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
