caebd6be6397f06e311f4973a0ad9dbc84c62942
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-03-10 13:56:23 -0500

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -23,7 +23,7 @@ const cardAppender = (selector) => {
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it with console.log!!).
   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
