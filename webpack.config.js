b650276ba1c48513dcee5d5f6bce45634727b44e
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-03-11 12:19:31 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,9 @@ module.exports = {
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },    compress: true,
     port: 3000,
   },
   module: {
