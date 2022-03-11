fac3e655fc01768708009acc16607ce877fbe977
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-03-11 12:18:53 -0500

    Update tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -19,7 +19,7 @@ const tabsAppender = (selector) => {
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
+  // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
