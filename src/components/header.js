fbb0d94e705f35691fab97797679ce4cf9a51e4c
Casey Harding <casey.r.harding@gmail.com>
2022-10-31 12:46:26 -0400

    Added explicit querySelector instructions.

--- src/components/header.js
+++ src/components/header.js
@@ -20,6 +20,12 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
+  // but it can also take in a variable (ie querySelector(selector))
+  // We are taking care of passing in the correct selector on line 16,
+  // so all that you need to do is pass it into the querySelector method
+  // for the tests to work!
 }
 
 export { Header, headerAppender }
