f9977b595bec3f78c604e3f416f130de9a19dd96
Orlando Castillo <omaddoc@gmail.com>
2023-04-27 09:35:31 -0400

    header finished

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,24 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+
+  const header = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const titleH1 = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+
+  header.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+
+  dateSpan.textContent = date;
+  titleH1.textContent = title;
+  tempSpan.textContent = temp;
+
+  header.append(dateSpan, titleH1, tempSpan);
+
+  return header;
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,12 +37,14 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-
-  // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
+  // HINT: querySelector can take in a string (ie querySelector("#wrapper"))
   // but it can also take in a variable (ie querySelector(selector))
   // We are taking care of passing in the correct selector on line 16,
   // so all that you need to do is pass it into the querySelector method
   // for the tests to work!
-}
 
-export { Header, headerAppender }
+  const wrapper = document.querySelector(selector);
+  wrapper.appendChild(Header('Weather', '04/27/23', '32'));
+};
+
+export { Header, headerAppender };
